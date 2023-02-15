62d025a4fe062fdee56c2450a724e971115e38ad
Etienne Bellerive-Blais <etiennebellerive@gmail.com>
2023-02-14 19:01:07 -0500

    sec

--- ProjetPacman1/ProjetPacman1.cpp
+++ ProjetPacman1/ProjetPacman1.cpp
@@ -2,11 +2,112 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include<dos.h>   //for delay()
+#include <windows.h>
+#include <conio.h>
+
+using namespace std;
+
+int nbLignes = 8;
+int nbColonnes = 10;
+char** matrice;
+enum SensEtat{gauche, droite, haut, bas};
+
+void lireMatrice(char** m)
+{
+    char sautDeLigne;
+    string filename = "map.txt";
+    ifstream file(filename);
+    if (!file.is_open()) {
+        std::cout << "Error opening file " << filename << std::endl;
+    }
+
+    for (int i = 0; i < nbLignes; i++)
+    {
+        for (int j = 0; j < nbColonnes; j++)
+        {
+            file.get(m[j][i]);
+        }
+        file.get(sautDeLigne);
+    }
+
+    file.close();
+}
+
+void AfficherMatrice(char** m)
+{
+    for (int i = 0; i < nbLignes; i++)
+    {
+        for (int j = 0; j < nbColonnes; j++)
+        {
+            cout << m[j][i];
+        }
+        cout << endl;
+    }
+}
 
 int main()
 {
+    
+    //cache le curseur
+    CONSOLE_CURSOR_INFO cursorInfo;
+    cursorInfo.dwSize = 100;
+    cursorInfo.bVisible = false;
+    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
+
+    SensEtat sens = gauche;
+    char** matrice = new char* [nbColonnes];
+    for (int i = 0; i < nbColonnes; i++) {
+        matrice[i] = new char[nbLignes];
+    }
+    
     std::cout << "Hello World!\n";
+    
+
+    //initialisation et affichage initial de la matrice
+    for (int i = 0; i < nbLignes; i++)
+    {
+        for (int j = 0; j < nbColonnes; j++)
+        {
+            matrice[j][i] = ' ';
+            cout << matrice[j][i];
+        }
+        cout << endl;
+    }
+    
+    lireMatrice(matrice);
+    AfficherMatrice(matrice);
+
+    char c = 'q';
+    int f = 0;
+    COORD cursorPosition;
+    cursorPosition.X = 0;
+    cursorPosition.Y = 0;
+    system("cls");
+    
+    while (c != ' ')
+    {
+        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cursorPosition);
+        
+        
+        
+        AfficherMatrice(matrice);
+       
+    }
+    
 }
+
+
+
+
+
+
+
+
+
+
 
 // Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
 // Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
