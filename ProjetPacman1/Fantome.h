040d0eca731d5b63653c7d11edc37f617e394685
Etienne Bellerive-Blais <etiennebellerive@gmail.com>
2023-02-16 22:30:30 -0500

    fantomefucké

--- ProjetPacman1/Fantome.h
+++ ProjetPacman1/Fantome.h
@@ -6,10 +6,14 @@
 #include <windows.h>
 #include <conio.h>
 #include "coordonnee.h"
+#include <ctime>
 
 using namespace std;
 
-enum SensEtat { gauche, droite, haut, bas, nul };
+enum SensEtat { haut, bas, droite, gauche, nul };
+
+double tprecedent;
+
 
 class Fantome
 {
@@ -18,27 +22,31 @@ private:
 	SensEtat sensChoisi;
 	Coordonnee position;
 public:
-	Fantome(int x, int y);
+	Fantome(int x, int y, char** matrice);
 	void determinerSensPossible(char** matrice);
 	void determinerSensChoisi();
 	SensEtat getSens();
-
+	void BougerFantome(char** matrice);
+	double getTempsSecondes();
+	double getTempsMs();
 };
 
-Fantome::Fantome(int x, int y)
+Fantome::Fantome(int x, int y, char** matrice)
 {
 	position.x = x;
 	position.y = y;
+	sensChoisi = nul;
+	matrice[position.x][position.y] = 'F';
 }
 
 void Fantome::determinerSensPossible(char** matrice)
 {
-	if (matrice[position.x][position.y + 1] != '#')
+	if (matrice[position.x][position.y - 1] != '#')
 		sensPossible[0] = haut;
 	else
 		sensPossible[0] = nul;
 
-	if (matrice[position.x][position.y - 1] != '#')
+	if (matrice[position.x][position.y + 1] != '#')
 		sensPossible[1] = bas;
 	else
 		sensPossible[1] = nul;
@@ -52,4 +60,66 @@ void Fantome::determinerSensPossible(char** matrice)
 		sensPossible[3] = gauche;
 	else
 		sensPossible[3] = nul;
+}
+
+SensEtat Fantome::getSens()
+{
+	return sensChoisi;
+}
+
+void Fantome::determinerSensChoisi()
+{
+	if (sensPossible[sensChoisi] != nul)
+		return;
+	
+	
+	srand(time(0));
+	int rand_int = rand() % 4;
+	while (sensPossible[rand_int] == nul)
+	{
+		rand_int = rand() % 4;
+	}
+	sensChoisi = sensPossible[rand_int];
+
+	
+			
+}
+
+void Fantome::BougerFantome(char** matrice)
+{
+	if (tprecedent + 100 < getTempsMs())
+	{
+		
+		determinerSensPossible(matrice);
+		determinerSensChoisi();
+		matrice[position.x][position.y] = ' ';
+		if (sensChoisi == gauche)
+			position.x--;
+		if (sensChoisi == droite)
+			position.x++;
+		if (sensChoisi == haut)
+			position.y--;
+		if (sensChoisi == bas)
+			position.y++;
+		
+		matrice[position.x][position.y] = 'F';
+		tprecedent+=100;
+	}
+}
+
+double Fantome::getTempsSecondes()
+{
+	static time_t start_time = time(NULL);
+	time_t current_time = time(NULL);
+	double elapsed_seconds = difftime(current_time, start_time);
+	return elapsed_seconds;
+}
+
+double Fantome::getTempsMs()
+{
+	static clock_t start_time = clock();
+	clock_t current_time = clock();
+	double elapsed_ticks = (double)(current_time - start_time);
+	double elapsed_milliseconds = (elapsed_ticks / CLOCKS_PER_SEC) * 1000.0;
+	return elapsed_milliseconds;
 }
--- ProjetPacman1/ProjetPacman1.cpp
+++ ProjetPacman1/ProjetPacman1.cpp
@@ -17,6 +17,7 @@ int nbColonnes = 10;
 char** matrice;
 
 
+
 void lireMatrice(char** m)
 {
     char sautDeLigne;
@@ -88,36 +89,17 @@ int main()
     cursorPosition.X = 0;
     cursorPosition.Y = 0;
     system("cls");
-    
+    Fantome *fantome = new Fantome(1, 1, matrice);
+    int compteur = 0;
     while (c != ' ')
     {
         SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cursorPosition);
         
         
-        
         AfficherMatrice(matrice);
+        fantome->BougerFantome(matrice);
+        cout << fantome->getTempsSecondes();
        
     }
     
 }
-
-
-
-
-
-
-
-
-
-
-
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
-
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
